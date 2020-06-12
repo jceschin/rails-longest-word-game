41fb5229b6944b202f09a427a9bd0788b34d0fb9
Juan Ceschin <juanceschin@gmail.com>
2020-06-11 21:29:18 -0300

    Last part remaining

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,26 @@
 class GamesController < ApplicationController
   def new
+    @letters = []
+    12.times do
+      @letters << ('A'..'Z').to_a.sample
+    end
   end
 
   def score
+    @words = params[:word]
+    @word = @words.last.split("")
+    @answer = ""
+
+    @word.each do |letter|
+      if !@letters.include?(letter)
+        @letters = @letters.join(",")
+        @answer = "Sorry but #{@word.join} can't be built out of #{@letters}"
+      end
+    end
+
+    if !@answer
+
+    end
   end
+
 end
