caa25057f8abec196f42fe7f03bfe3078204f17d
Juan Ceschin <juanceschin@gmail.com>
2020-06-14 12:31:55 -0300

    Add score page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @letters = []
@@ -7,19 +9,34 @@ class GamesController < ApplicationController
   end
 
   def score
-    @word = params[:word].last.split("")
-    @answer = ""
+    # Take word and letters
+    @word = params[:word].upcase
+    @letters = params[:letters].split
+    # Check if word can be built from original grid (word vs letters)
+    @included = included?(@word, @letters)
 
-    @word.each do |letter|
-      if !@letters.includes?(letter)
-        @letters = @letters.join(",")
-        @answer = "Sorry but #{@word.join} can't be built out of #{@letters}"
-      end
-    end
+    # Check if its an english word
+    @english_word = english_word?(@word)
 
-    if !@answer
-
+    # Create answer
+    if !@included
+      @answer = "Sorry but #{@word} cant be build out of #{@letters.join(',')}"
+    elsif !@english_word
+      @answer = "Sorry but #{@word} does not seem to be valid english word"
+    else
+      @answer = "Congratulations! #{@word} is a valid English word!"
     end
   end
 
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
 end
